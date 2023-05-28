68ac655b2ca12234a00870f5942be04c3b93374b
AnaGil27 <gilyalta27@gmail.com>
2023-05-28 16:34:40 -0500

    repositorio creado

---
+++ src/main/java/pe/edu/cibertec/CasoLaboratorio2Grupo7/CasoLaboratorio2Grupo7Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CasoLaboratorio2Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoLaboratorio2Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoLaboratorio2Grupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CasoLaboratorio2Grupo7/CasoLaboratorio2Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CasoLaboratorio2Grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoLaboratorio2Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
